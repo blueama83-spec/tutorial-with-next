6cf439b48a8e737c85621471c77493e4e3c12c88
blueama83-spec <blueama83@gmail.com>
2026-05-06 11:24:35 +0100

    Delete main.ts

--- main.ts
+++
@@ -1,6 +0,0 @@
-Deno.serve((req: Request) => {
-  const url = new URL(req.url);
-  return new Response(`Hello from Deno Deploy! You visited: ${url.pathname}`, {
-    headers: { "content-type": "text/plain" },
-  });
-});
